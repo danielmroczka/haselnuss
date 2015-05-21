85920e9eb5766f50ab2088a1e5ac873d18cba89d
danielmroczka <daniel.mroczka@gmail.com>
2015-05-21 14:27:41 +0200

    cleanup

--- jkvdb-example/src/main/java/com/labs/dm/jkvdb/example/file/Main.java
+++ jkvdb-example/src/main/java/com/labs/dm/jkvdb/example/file/Main.java
@@ -16,24 +16,24 @@ public class Main
     public static void main(String[] args) throws IOException
     {
         long time = System.nanoTime();
-        IFileStorage storage = new FastFileMapStorage(".", "test4");
+        IFileStorage storage = new FastFileMapStorage(".", "test5");
 
         for (int i = 0; i < COUNT; i++)
         {
-            storage.put(i, i%10);
+            storage.put(i, "abcdefghijklmnoprtstuwxyz" + i);
         }
         time = System.nanoTime() - time;
-        System.out.println("put in   \t" + time/1e6 + " [ms] \t" + COUNT / (time / 1e9) + " op/s");
+        System.out.println("put in   \t" + Math.round(time/1e6) + " [ms] \t" + Math.round(COUNT / (time / 1e9)) + " op/s");
 
         time = System.nanoTime();
         storage.flush();
         time = System.nanoTime() - time;
-        System.out.println("flush in \t" + time/1e6 + " [ms] \t" + COUNT / (time / 1e9) + " op/s");
+        System.out.println("flush in \t" + Math.round(time/1e6) + " [ms] \t" + Math.round(COUNT / (time / 1e9)) + " op/s");
 
         time = System.nanoTime();
         storage.load();
         time = System.nanoTime() - time;
-        System.out.println("load in \t" + time / 1e6 + " [ms] \t" + COUNT / (time / 1e9) + " op/s");
+        System.out.println("load in \t" + Math.round(time / 1e6) + " [ms] \t" + Math.round(COUNT / (time / 1e9)) + " op/s");
 
         System.out.println(storage.get(95));
 
