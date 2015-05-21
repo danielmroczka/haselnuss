6738b01e3fc01b5e2f83a51e0c4c6fe557c75c81
daniel <daniel.mroczka@gmail.com>
2015-05-21 08:21:43 +0200

    refactoring

--- jkvdb-core/src/main/java/com/labs/dm/jkvdb/core/hashmap/SuperFastFileMapStorage.java
+++ jkvdb-core/src/main/java/com/labs/dm/jkvdb/core/hashmap/SuperFastFileMapStorage.java
@@ -12,12 +12,11 @@ import java.util.logging.Logger;
  */
 public class SuperFastFileMapStorage extends AbstractHashMapStorage implements Serializable, IFileStorage {
     private static final Logger logger = Logger.getLogger(SuperFastFileMapStorage.class.getSimpleName());
-    protected final String filename;
     private static  final int BUFFER_SIZE = 64000;
+    protected final String filename;
 
     public SuperFastFileMapStorage(String dir, String name) {
         this.filename = dir + File.separator + name;
-
     }
 
     public SuperFastFileMapStorage(String name) {
@@ -27,43 +26,40 @@ public class SuperFastFileMapStorage extends AbstractHashMapStorage implements S
     @Override
     public void flush() {
 
-        try (ObjectOutputStream dos = new ObjectOutputStream(new BufferedOutputStream(
+        try (ObjectOutputStream oos = new ObjectOutputStream(new BufferedOutputStream(
                  (new FileOutputStream(new RandomAccessFile(filename, "rw").getFD())), BUFFER_SIZE))) {
 
-            dos.writeInt(map.size());
+            oos.writeInt(map.size());
 
             for (Map.Entry<Serializable, Serializable> entry : map.entrySet()) {
-                dos.writeObject(entry.getKey());
-                dos.writeObject(entry.getValue());
+                oos.writeObject(entry.getKey());
+                oos.writeObject(entry.getValue());
             }
 
-        } catch (IOException e1) {
-            logger.severe(e1.getMessage());
+        } catch (IOException ex) {
+            logger.severe(ex.getMessage());
         }
-
 
     }
 
     @Override
     public void load() {
 
-        try (ObjectInputStream dos = new ObjectInputStream(new BufferedInputStream(
+        try (ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(
               (new FileInputStream(new RandomAccessFile(filename, "r").getFD())), BUFFER_SIZE))) {
 
-            int size = dos.readInt();
+            int size = ois.readInt();
             map = new HashMap<>(size);
 
             for (int item = 0; item < size; item++) {
-                Serializable key = (Serializable) dos.readObject();
-                Serializable val = (Serializable) dos.readObject();
+                Serializable key = (Serializable) ois.readObject();
+                Serializable val = (Serializable) ois.readObject();
                 map.put(key, val);
             }
-
 
         } catch (IOException | ClassNotFoundException e1) {
             logger.severe(e1.getMessage());
         }
-
     }
 
     @Override
