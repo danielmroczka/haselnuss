c57f3c1723e407e6cd2403e33df63177ee47ab26
daniel <daniel.mroczka@gmail.com>
2015-06-19 13:24:05 +0200

    cleanup

--- haselnuss-core/src/main/java/com/labs/dm/haselnuss/server/IProvider.java
+++ haselnuss-core/src/main/java/com/labs/dm/haselnuss/server/IProvider.java
@@ -1,12 +1,23 @@
 package com.labs.dm.haselnuss.server;
 
+import com.labs.dm.haselnuss.Consts;
+
 import java.io.IOException;
 
 /**
  * Created by daniel on 2015-06-12.
  */
 public interface IProvider {
+
+    enum TYPE {
+        EMBEDDED,
+        HTTP,
+        TCP
+    }
+
     void start() throws IOException;
 
     void close() throws IOException;
+
+    Consts.SERVER_STATUS status();
 }
